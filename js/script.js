e3ee1e557db0ab4af709c5c3a824da2ab09405a1
webcoding.by <webcoding.by@gmail.com>
2020-12-07 12:46:28 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const sliderButtons = document.querySelectorAll('.button__slider'),
+  next = document.querySelector('.bi-arrow-right'),
+  prev = document.querySelector('.bi-arrow-left');
+
+
+next.addEventListener('.click', function () {
+  next.classList.add('button__slider--color');
+})
+
+
