7a566991f93789258f3668097e9707ebec52ab17
webcoding.by <webcoding.by@gmail.com>
2020-12-08 13:33:46 +0300

    add next button clicked

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const sliderButtons = document.querySelectorAll('.button__slider'),
   next = document.querySelector('.bi-arrow-right'),
-  prev = document.querySelector('.bi-arrow-left');
+  prev = document.querySelector('.bi-arrow-left'),
+  blockItem = document.querySelector('.documents__item');
 
 
-next.addEventListener('.click', function () {
-  next.classList.add('button__slider--color');
-})
 
+if (next || prev) {
+  next.addEventListener('click', function () {
+    next.classList.add('button__slider--color');
+    prev.classList.remove('button__slider--color');
+    blockItem.classList.remove('documents__item--left');
+    blockItem.classList.remove('documents__item--right');
+    blockItem.classList.add('documents__item--left');
+  })
+
+  prev.addEventListener('click', function () {
+    prev.classList.add('button__slider--color');
+    next.classList.remove('button__slider--color');
+    blockItem.classList.remove('documents__item--left');
+    blockItem.classList.add('documents__item--right');
+  })
+}
 
